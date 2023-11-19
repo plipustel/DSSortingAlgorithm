cda52000c4bff4709eb35bb13a631c7c9543da95
Plipus Telaumbanua <plipus.tel@gmail.com>
2023-11-19 18:18:59 +0700

    MergeSort on progress

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -7,46 +7,44 @@
  */
 public class MergeSort {
 
-	/**
-	 * @param args
-	 */
-	public static int[] mSort(int[] nums) {
-		return nums;
-
-	}
-
-	public static int factorial(int n) {
-		if (n == 0) {
-			return 1;
-		}
-
-		return n * factorial(n - 1);
-	
-	}
-
 	public static void main(String[] args) {
+		int[] nums = { 12, 90, 14, 23, 48, 21, 10, 11, 15, 11, 33, 23, 23, 44, 42, 23}; // length 6/2 = 3;
+
+		System.out.println("Unsorted nums array of integer:");
+		for (int arr : nums) {
+			System.out.print(arr + " ");
+		}
+
+		System.out.println("");
+		mSortDivide(nums);
+
+	}
+
+	private static void mSortDivide(int[] nums) {
 		// TODO Auto-generated method stub
-		int[] nums = { 12, 90, 14, 23, 48, 21 };
 
-		mSort(nums);
-
-		boolean j = false;
-		int i = 0;
-
-		while (!j) {
-			System.out.println(i);
-			i++;
-
-			if (i == 18) {
-				j = true;
-			}
+		if (nums.length <= 1) {
+			return;
 		}
 
-		System.out.println(factorial(5));
+		int middle = nums.length / 2; // middle = 3
 
-		for (int num : nums) {
-			System.out.print(num + " ");
+		int[] leftArray = new int[middle];
+		int[] rigthArray = new int[nums.length - middle];
+
+		for (int i = 0; i < leftArray.length; i++) {
+			leftArray[i] = nums[i];
 		}
+
+		// fill left array with array nums items
+		for (int j = 0; j < rigthArray.length; j++) {
+			rigthArray[j] = nums[middle++];
+		}
+	
+		mSortDivide(leftArray); // call itselft recursive
+		mSortDivide(rigthArray);
+		
+		
 	}
 
 }
